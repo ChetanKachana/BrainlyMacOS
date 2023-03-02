3757bc6ccee5d33b85217e2cd5aca144742b5bd6
Chetan Kachana <chetan.kachana@gmail.com>
2023-03-01 19:37:46 -0800

    Initial Commit

---
+++ Brainly/BrainlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainlyApp.swift
+//  Brainly
+//
+//  Created by Chetan Kachana on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct BrainlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
